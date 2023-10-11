dd4b8b41f3657aa9f40811889bfa625cfae3b1ac
Cleilson-Andrade <cleilson.andrade@anjunexpress.com.br>
2023-10-11 11:32:59 -0300

    created method create task

---
+++ src/main/java/br/com/cleilsonandrade/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.cleilsonandrade.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
